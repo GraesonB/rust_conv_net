238ca6be38a1afb4b3adbf17eb1a5ac4061bbb37
Graeson Bergen <graesonbergen@gmail.com>
2022-10-16 08:34:04 +0900

    init

---
+++ matrix_operations/src/lib.rs
@@ -0,0 +1,4 @@
+
+pub fn multiply_by_2(num: &i32) -> i32 {
+    return num * 2
+}
---
+++ model/src/main.rs
@@ -0,0 +1,7 @@
+use matrix_operations;
+
+
+fn main() {
+    let new_num =matrix_operations::multiply_by_2(&2);
+    println!("new num: {}", &new_num);
+}
