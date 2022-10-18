caa4bc00d8e5b5f8ce5c5c9659c2b110ac637672
Graeson Bergen <graesonbergen@gmail.com>
2022-10-18 14:08:51 +0900

    comments

--- matrix_operations/src/lib.rs
+++ matrix_operations/src/lib.rs
@@ -1,6 +1,6 @@
-use std::{fmt::Debug, ops::{Mul, Add, AddAssign}};
+use std::ops::AddAssign;
 
-#[cfg(test)] //cfg is conditional compilation
+#[cfg(test)] // cfg is conditional compilation
 mod tests;
 mod hdarray;
 use num::{Num, Zero};
@@ -8,18 +8,27 @@ use num::{Num, Zero};
 pub use self::hdarray::Array2D;
 
 
-pub fn dot<T: Num + AddAssign + Copy + Zero>(a: Array2D<T>, b:Array2D<T>) -> Array2D<T> {
+pub fn dot<T>(a: Array2D<T>, b:Array2D<T>) -> Array2D<T> 
+    where T: Num + AddAssign + Copy + Zero
+{
     if a.shape.1 != b.shape.0 {
         panic!("[ERROR] Incomaptible matrix shapes")
     }
+    // initialize array to be returned
     let mut product: Array2D<T> = Array2D::new((a.shape.0, b.shape.1));
-    for i in 0..product.shape.0 as usize {
-        for j in 0..product.shape.1 as usize {
+    // i and j are for looping through each position in the product array
+    for row in 0..product.shape.0 {
+        for col in 0..product.shape.1 {
+            // initialize sum and loop through a's row and b's column adding each product to sum
             let mut sum: T = T::zero();
-            for w in 0..a.shape.1 as usize {
-                sum += a.array[i][w] * b.array[w][j];
+            for i in 0..a.shape.1 {
+                // i moves along a's row and b's column. once finished, it moves over one column for b and repeats the process
+                // until the next row, starting at the first column again.
+                sum += a.array[row][i] * b.array[i][col];
             }
-            product.array[i].push(sum);
+            // append sum to the current row of the array. "col" isn't needed because we always just push it to the next value
+            // if we initialized product using Array2D::zeroes we could probably do product.array[row][col] = sum;
+            product.array[row].push(sum);
         }
     }
     product
--- model/src/main.rs
+++ model/src/main.rs
@@ -5,7 +5,7 @@ use std::time::{Duration, Instant};
 fn main() {
     let now = Instant::now();
     let array1 = Array2D::<i32>::ones((7500,15));
-    let array2 = Array2D::<i32>::ones((15,10000));
+    let array2 = array1.transpose();
     let product = dot(array1, array2);
-    println!("{}", now.elapsed().as_millis());
+    println!("{:?}", &product.array);
 }
